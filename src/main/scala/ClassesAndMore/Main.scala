8391df4d8497908d8ab7e04e7360588694a41a26
pardium <iludhankhar58@gmail.com>
2023-04-06 23:13:16 +0530

    just practice regular classes and case classes

---
+++ src/main/scala/ClassesAndMore/Main.scala
@@ -0,0 +1,45 @@
+package ClassesAndMore
+
+case class BankAccount(AccNo : Int , var Balance : Int = 0, InterestRate : Int) {
+
+
+  def deposit(amount: Int): Unit = {
+//    new BankAccount(AccNo, Balance + amount, InterestRate)
+
+    this.Balance+=amount
+
+    println("Hurray! amount added successfully")
+
+  }
+
+  def withdrow(amount: Int): Unit = {
+//    new BankAccount(AccNo, Balance - amount, InterestRate)
+    this.Balance-=amount
+    println("Hope you are happy with our service")
+  }
+
+  def calculateInterest(): Int = {
+
+    var result = this.Balance * this.InterestRate / 100
+    result
+
+  }
+}
+  object Account{
+    def main(args: Array[String]): Unit = {
+
+      var person:List[BankAccount] = List(BankAccount(1,10000,10))
+
+
+      person(0).deposit(500)
+      println(person(0).Balance)
+      println(person(0).calculateInterest())
+      println(person)
+
+
+
+
+    }
+  }
+
+
